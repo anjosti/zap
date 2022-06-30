16a8d8e8b45c9bd24909f68b63967d9f1c62c607
owenbrasil <103150476+owenbrasil@users.noreply.github.com>
2022-06-30 13:32:42 -0300

    Update index.js

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -24,6 +24,8 @@ import UserModal from "../components/UserModal";
 import { AuthContext } from "../context/Auth/AuthContext";
 import BackdropLoading from "../components/BackdropLoading";
 import { i18n } from "../translate/i18n";
+import logo from "../assets/logo.png";
+import { versionSystem } from "../../package.json";
 
 const drawerWidth = 240;
 
@@ -177,6 +179,7 @@ const LoggedInLayout = ({ children }) => {
         open={drawerOpen}
       >
         <div className={classes.toolbarIcon}>
+          <img src={logo} width={"80%"} style={{ marginLeft: 'auto' ,marginRight:'auto', display:'flex'}} />
           <IconButton onClick={() => setDrawerOpen(!drawerOpen)}>
             <ChevronLeftIcon />
           </IconButton>
@@ -217,8 +220,10 @@ const LoggedInLayout = ({ children }) => {
             noWrap
             className={classes.title}
           >
-            WhaTicket
+            Whaticket - v { versionSystem }
+            
           </Typography>
+
           {user.id && <NotificationsPopOver />}
 
           <div>
