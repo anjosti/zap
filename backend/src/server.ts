d98dcab0a70e078820617874db67098c229903f8
owenbrasil <103150476+owenbrasil@users.noreply.github.com>
2022-06-30 21:21:23 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,12 @@ import { initIO } from "./libs/socket";
 import { logger } from "./utils/logger";
 import { StartAllWhatsAppsSessions } from "./services/WbotServices/StartAllWhatsAppsSessions";
 
+import swaggerUi from "swagger-ui-express";
+
+import swaggerDocs from "./swagger.json"
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 const server = app.listen(process.env.PORT, () => {
   logger.info(`Server started on port: ${process.env.PORT}`);
 });
