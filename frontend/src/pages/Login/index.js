73a854d01c845cdbc8f691c65b99f9300ea5f2c6
owenbrasil <103150476+owenbrasil@users.noreply.github.com>
2022-07-07 15:14:09 -0300

    Update index.js

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,133 +1,99 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 import {
-  Button,
-  CssBaseline,
-  TextField,
-  Grid,
-  Box,
-  Typography,
-  Container,
-  InputAdornment,
-  IconButton,
-  Link
+    TextField,
+    InputAdornment,
+    IconButton
 } from '@material-ui/core';
 
-import { Visibility, VisibilityOff } from '@material-ui/icons';
-import { makeStyles } from "@material-ui/core/styles";
+import { LockOutlined, Visibility, VisibilityOff } from '@material-ui/icons';
+
+import './style.css';
+
 import { i18n } from "../../translate/i18n";
 
 import { AuthContext } from "../../context/Auth/AuthContext";
+
+import wave from './img/wave.png'
+import bg from './img/bg.svg'
+//import avatar from './img/avatar.svg'
 import { versionSystem } from "../../../package.json";
 import { nomeEmpresa } from "../../../package.json";
 import logo from '../../assets/logo.png';
 
-const Copyright = () => {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      © {new Date().getFullYear()}
-      {" - "}
-      <Link color="inherit" href="#">
-        { nomeEmpresa } - v { versionSystem }
-      </Link>
-      {"."}
-    </Typography>
-  );
-};
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
 const Login = () => {
-  const classes = useStyles();
+    const [user, setUser] = useState({ email: "", password: "" });
+    const [showPassword, setShowPassword] = useState(false);
 
-  const [user, setUser] = useState({ email: "", password: "" });
-  const [showPassword, setShowPassword] = useState(false);
+    const { handleLogin } = useContext(AuthContext);
 
-  const { handleLogin } = useContext(AuthContext);
+    const handleChangeInput = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
 
-  const handleChangeInput = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
-  };
+    const handlSubmit = (e) => {
+        console.log(user)
+        e.preventDefault();
+        handleLogin(user);
+    };
 
-  const handlSubmit = (e) => {
-    e.preventDefault();
-    handleLogin(user);
-  };
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <img alt="logo" src={logo}></img>
-        <form className={classes.form} noValidate onSubmit={handlSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label={i18n.t("login.form.email")}
-            name="email"
-            value={user.email}
-            onChange={handleChangeInput}
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label={i18n.t("login.form.password")}
-            id="password"
-            value={user.password}
-            onChange={handleChangeInput}
-            autoComplete="current-password"
-            type={showPassword ? 'text' : 'password'}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={() => setShowPassword((e) => !e)}
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              )
-            }}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            {i18n.t("login.buttons.submit")}
-          </Button>
-          
-        </form>
-      </div>
-      <Box mt={8}><Copyright /></Box>
-    </Container>
-  );
+    return (
+        <>
+            <img className="wave" src={wave} />
+            <div className="container">
+                <div className="img">
+                    <img src={bg} />
+                </div>
+                <div className="login-content">
+                    <form noValidate onSubmit={handlSubmit}>
+                        <img src={logo} width={"80%"} style={{ marginLeft: 'auto' ,marginRight:'auto', display:'flex'}} />
+                        <TextField
+                            variant="standard"
+                            margin="normal"
+							color="warning"
+                            required
+                            fullWidth
+                            id="email"
+                            label={i18n.t("login.form.email")}
+                            name="email"
+                            value={user.email}
+                            onChange={handleChangeInput}
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="standard"
+                            margin="normal"
+							color="success"
+                            required
+                            fullWidth
+                            name="password"
+                            label={i18n.t("login.form.password")}
+                            id="password"
+                            value={user.password}
+                            onChange={handleChangeInput}
+                            autoComplete="current-password"
+                            type={showPassword ? 'text' : 'password'}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={() => setShowPassword((e) => !e)}
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                        <input type="submit" className="btn" value="Acessar" />
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Login;
