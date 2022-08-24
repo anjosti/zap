3bbf47d86dbb8da1d47ce7fb2658f6e4e021c92d
Andresilvia <110529443+Andresilvia@users.noreply.github.com>
2022-08-24 10:20:22 -0300

    Update 20200904070004-create-default-users.ts

--- backend/src/database/seeds/20200904070004-create-default-users.ts
+++ backend/src/database/seeds/20200904070004-create-default-users.ts
@@ -7,7 +7,7 @@ module.exports = {
       [
         {
           name: "Administrador",
-          email: "admin@wadmin.com.br",
+          email: "admin@admin.com.br",
           passwordHash: "$2a$08$WaEmpmFDD/XkDqorkpQ42eUZozOqRCPkPcTkmHHMyuTGUOkI8dHsq",
           profile: "admin",
           tokenVersion: 0,
