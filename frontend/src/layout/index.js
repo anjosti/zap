b5d1b8154e245a98312d26ed7ab09e0d7f048f9e
Andresilvia <110529443+Andresilvia@users.noreply.github.com>
2022-09-10 19:37:55 -0300

    Add files via upload

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -19,14 +19,12 @@ import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 
 import MainListItems from "./MainListItems";
-
+import NotificationsPopOver from "../components/NotificationsPopOver";
 import UserModal from "../components/UserModal";
 import { AuthContext } from "../context/Auth/AuthContext";
 import BackdropLoading from "../components/BackdropLoading";
+import logodash from "../assets/logo-dash.png";
 import { i18n } from "../translate/i18n";
-import logo from "../assets/logo.png";
-import { versionSystem } from "../../package.json";
-import { nomeEmpresa } from "../../package.json";
 
 const drawerWidth = 240;
 
@@ -180,7 +178,7 @@ const LoggedInLayout = ({ children }) => {
         open={drawerOpen}
       >
         <div className={classes.toolbarIcon}>
-          <img src={logo} width={"80%"} style={{ marginLeft: 'auto' ,marginRight:'auto', display:'flex'}} />
+		<img src={logodash} alt="logo" />
           <IconButton onClick={() => setDrawerOpen(!drawerOpen)}>
             <ChevronLeftIcon />
           </IconButton>
@@ -221,11 +219,9 @@ const LoggedInLayout = ({ children }) => {
             noWrap
             className={classes.title}
           >
-            { nomeEmpresa } - v { versionSystem }
-            
+            Gallera.net | Seu WhatsApp Web em vários computadores!
           </Typography>
-
-          
+          {user.id && <NotificationsPopOver />}
 
           <div>
             <IconButton
